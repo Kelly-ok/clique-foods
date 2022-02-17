7aa528fbbac7bd2ce337680af04db22bff55cff0
kelvin <kelvinomenyi@gmail.com>
2022-02-17 20:18:23 +0300

    first commit

--- src/redux/models/index.ts
+++ src/redux/models/index.ts
@@ -1,52 +1,51 @@
-import {Address} from 'expo-location'
+import {} from "expo-location";
 //Category
 
-export interface Category{
-    title: String,
-    icon: String
+export interface Category {
+  title: String;
+  icon: String;
 }
 
 // Food Model
-export interface FoodModel{
-    _id : string,
-    name: string,
-    description:string,
-    category: string,
-    price:number,
-    readyTime:number,
-    image:[string]
+export interface FoodModel {
+  _id: string;
+  name: string;
+  description: string;
+  category: string;
+  price: number;
+  readyTime: number;
+  image: [string];
 }
 
 // Restaurant Model
-export interface Restaurant{
-    _id: string,
-    name:string,
-    foodType:string,
-    address:string,
-    phone:string,
-    images:string,
-    food:[FoodModel];
+export interface Restaurant {
+  _id: string;
+  name: string;
+  foodType: string;
+  address: string;
+  phone: string;
+  images: string;
+  food: [FoodModel];
 }
 
 // FoodAvailability
-export interface FoodAvailability{
-    categories:[Category];
-    restaurants:[Restaurant];
-    foods:[FoodModel]
+export interface FoodAvailability {
+  categories: [Category];
+  restaurants: [Restaurant];
+  foods: [FoodModel];
 }
 
-// todo : Modify later
 // User Model
-export interface UserModel{
-    firstName:string;
-    lastName:string;
-    contactNumber:string;
-    token:string
+export interface UserModel {
+  firstName: string;
+  lastName: string;
+  contactNumber: string;
+  token: string;
 }
 
 //user state model
-export interface UserState{
-    user:UserModel;
-    location: Address;
-    error:string | undefined
+export interface UserState {
+  user: UserModel;
+
+  error: string | undefined;
 }
